5b66edf98896bff5909a3408987bf1301ce9d6ca
cpudvini <cpudvini@gmail.com>
2024-04-01 11:52:05 -0300

    Add info de contato e privacidade

--- app/src/main/java/com/evosouza/news/data/model/Articles.kt
+++ app/src/main/java/com/evosouza/news/data/model/Articles.kt
@@ -9,14 +9,14 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null,
-    val source: Source?,
-    val author: String?,
-    val title: String?,
-    val description: String?,
-    val url: String?,
-    val urlToImage: String?,
-    val publishedAt: String?,
-    val content: String?,
+    val source: Source? = null,
+    val author: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val url: String? = null,
+    val urlToImage: String? = null,
+    val publishedAt: String? = null,
+    val content: String? = null,
     var userId: Long
 ): Serializable
 
--- app/src/main/java/com/evosouza/news/ui/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/evosouza/news/ui/home/profile/ProfileFragment.kt
@@ -1,7 +1,9 @@
 package com.evosouza.news.ui.home.profile
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,7 @@ import com.evosouza.news.R
 import com.evosouza.news.core.Status
 import com.evosouza.news.data.database.NewsDB
 import com.evosouza.news.data.database.repository.UserRepositoryImpl
+import com.evosouza.news.data.model.Article
 import com.evosouza.news.data.model.User
 import com.evosouza.news.data.sharedpreference.SharedPreference
 import com.evosouza.news.databinding.FragmentProfileBinding
@@ -29,20 +32,7 @@ class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private lateinit var viewModel: ProfileViewModel
-    private lateinit var user: User
-    private var imageBitmap: Bitmap? = null
-    private val getContent =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            uri?.let {
-                try {
-                    val imaStream = requireActivity().contentResolver.openInputStream(uri)
-                    imageBitmap = BitmapFactory.decodeStream(imaStream)
-                    binding.profileImage.setImageBitmap(imageBitmap)
-                } catch (e: Exception) {
-                    Timber.e(e)
-                }
-            }
-        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -62,129 +52,34 @@ class ProfileFragment : Fragment() {
             cache
         ).create(ProfileViewModel::class.java)
 
-        getUserData()
-        setupObservers()
         setupClickListener()
-
     }
 
     private fun setupClickListener() {
-        binding.txtChangeData.setOnClickListener {
-            showFormData()
-        }
-
         binding.btnChangeInterests.setOnClickListener {
             openSubjectsFragments()
         }
 
-        binding.imgCamera.setOnClickListener {
-            chooseImage()
-        }
-
-        binding.btnSaveData.setOnClickListener {
-            binding.apply {
-                viewModel.validateUserInput(
-                    formData.edtUserName.text.toString(),
-                    formData.edtUserEmail.text.toString(),
-                    formData.edtLastUserPassword.text.toString(),
-                    formData.edtNewUserPassword.text.toString(),
-                    imageBitmap?.bitmapToString()
+        binding.site.setOnClickListener{
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://studioappnews.blogspot.com/p/app-news.html")
                 )
-            }
+            )
         }
 
-        binding.formData.cancelButton.setOnClickListener {
-            showDataText()
+        binding.policy.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://studioappnews.blogspot.com/p/privacy-policy.html")
+                )
+            )
         }
     }
 
     private fun openSubjectsFragments() {
         findNavController().navigate(R.id.action_profileFragment_to_subjectChoseFragment)
-    }
-
-    private fun setupObservers() {
-        viewModel.updateUserData.observe(viewLifecycleOwner) { done ->
-            when (done.status) {
-                Status.ERROR -> {
-                    showDataText()
-                    binding.progressBar.visibility = View.GONE
-                }
-                Status.SUCCESS -> {
-                    getUserData()
-                    showDataText()
-                    binding.progressBar.visibility = View.GONE
-                }
-                Status.LOADING -> {
-                    binding.progressBar.visibility = View.VISIBLE
-                }
-            }
-        }
-
-        viewModel.lastPassword.observe(viewLifecycleOwner) {
-            binding.formData.inputLayoutLastUserPassword.setErrorResId(requireContext(), it)
-        }
-
-        viewModel.newPassword.observe(viewLifecycleOwner) {
-            binding.formData.inputNewUserPassword.setErrorResId(requireContext(), it)
-        }
-
-        viewModel.userEmail.observe(viewLifecycleOwner) {
-            binding.formData.inputLayoutUserEmail.setErrorResId(requireContext(), it)
-        }
-
-        viewModel.userName.observe(viewLifecycleOwner) {
-            binding.formData.inputLayoutUserName.setErrorResId(requireContext(), it)
-        }
-    }
-
-    private fun getUserData() {
-        viewModel.getUserData().observe(viewLifecycleOwner) { userData ->
-            user = userData
-            fillUserFields(user)
-            fillEdtUser(user)
-            user.photo?.let { setImageResourceFromPath(it) }
-        }
-    }
-
-    private fun fillUserFields(user: User) {
-        binding.apply {
-            textData.txtUserEmail.text = resources.getString(R.string.email, user.email)
-            textData.txtUserName.text = resources.getString(R.string.username, user.userName)
-            textData.txtUserPassword.text =
-                resources.getString(R.string.password, user.password.toStars())
-        }
-    }
-
-    private fun setImageResourceFromPath(path: String) {
-        if (path.isEmpty() || path.isBlank()) return
-        val bitmap = path.stringBase64ToBitmap()
-        binding.profileImage.setImageBitmap(bitmap)
-    }
-
-    private fun fillEdtUser(user: User) {
-        binding.apply {
-            formData.edtUserEmail.setText(user.email)
-            formData.edtUserName.setText(user.userName)
-        }
-    }
-
-    private fun chooseImage() = getContent.launch("image/*")
-
-    private fun showDataText() {
-        binding.textData.root.visibility = View.VISIBLE
-        binding.btnChangeInterests.visibility = View.VISIBLE
-        binding.txtChangeData.visibility = View.VISIBLE
-        binding.btnSaveData.visibility = View.GONE
-        binding.formData.root.visibility = View.GONE
-        binding.imgCamera.visibility = View.GONE
-    }
-
-    private fun showFormData() {
-        binding.textData.root.visibility = View.GONE
-        binding.btnChangeInterests.visibility = View.GONE
-        binding.txtChangeData.visibility = View.GONE
-        binding.btnSaveData.visibility = View.VISIBLE
-        binding.formData.root.visibility = View.VISIBLE
-        binding.imgCamera.visibility = View.VISIBLE
     }
 }
